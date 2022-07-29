f115bb324952c2cc080f4743fb2bb30cffa58315
ngjingkang <ngjingkg@gmail.com>
2022-07-29 22:16:42 +0800

    login prototype

--- java assignment sem 4/Java assjgnment sem  4/src/login.java
+++ java assignment sem 4/Java assjgnment sem  4/src/login.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 public class login {
-    private String username;
-    private String password;
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Username");
+            String uname = sc.nextLine();
+            System.out.println("Enter the Password");
+            String upass = sc.nextLine();
+
+            if(uname.equals(realname) && upass.equals(realpass)){ //realname and realpass are from database to link
+                System.out.println("Welcome "+uname+" you have Logged-in Successfully");
+            }
+            else{
+                System.out.println("Username or password Mismatch");
+            }
+        }
+    }
+
--- java assignment sem 4/Java assjgnment sem  4/src/posMainPage.java
+++ java assignment sem 4/Java assjgnment sem  4/src/posMainPage.java
@@ -1,4 +1,4 @@
 
-public class posMainPage() {
+//public class posMainPage() {
 
-}
+
