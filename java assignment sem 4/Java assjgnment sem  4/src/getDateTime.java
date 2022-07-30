4b74e0649e5bd141d267fa460ed8256a12d0610c
Ng Yu Shun <ngyushun@gmail.com>
2022-07-30 20:53:24 +0800

    added inheritance into login page

--- java assignment sem 4/Java assjgnment sem  4/src/getDateTime.java
+++
@@ -1,7 +0,0 @@
-import java.text.SimpleDateFormat;
-
-//public class getDateTime(){
-    //public getDateTime() {
-        //return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-  //  }
-//}
--- java assignment sem 4/Java assjgnment sem  4/src/login.java
+++ java assignment sem 4/Java assjgnment sem  4/src/login.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public class login {
-        public static void main(String[] args) {
-            int count;
-            do {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Enter the Username");
-                String uname = sc.nextLine();
-                System.out.println("Enter the Password");
-                String upass = sc.nextLine();
-                String realname = "ngjingkang";
-                String realpass = "123456";
-                count = 0;
-                if (uname.equals(realname) && upass.equals(realpass)) { //realname and realpass are from database to link
-                    System.out.println("Welcome " + uname + " you have Logged-in Successfully");
-                } else {
-                    System.out.println("Username or password Mismatch");
-                    count++;
-                }
-            } while (count == 1);
+    private String realname;
+    private String realpass;
 
-        }
+    public String getRealname() {
+            return realname;
     }
+
+    public void setRealname(String realname) {
+            this.realname = realname;
+    }
+
+    public String getRealpass() {
+            return realpass;
+    }
+
+    public void setRealpass(String realpass) {
+            this.realpass = realpass;
+    }
+
+
+}
 
