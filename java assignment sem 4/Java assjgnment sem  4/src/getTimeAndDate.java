c4584e380507985f76cfe8cf4b15aaa029631193
Ng Yu Shun <ngyushun@gmail.com>
2022-09-22 22:45:19 +0800

    yes

--- java assignment sem 4/Java assjgnment sem  4/src/getTimeAndDate.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-//function to retrieve TimeAndDate and return TimeAndDate
-public class getTimeAndDate {
-        public static LocalDateTime TimeDate() {
-            return LocalDateTime.now();
-        }
-}
-
